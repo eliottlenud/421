633cfe8687ccf8208c1ed4adda610c45e15471dc
eliottlenud <eliott.le-nud@hei.yncrea.fr>
2020-03-06 14:34:36 +0100

    menu + add joueur

--- app/src/main/java/io/hei/a421/MainActivity.java
+++ app/src/main/java/io/hei/a421/MainActivity.java
@@ -17,10 +17,12 @@ import io.hei.a421.Models.Joueur;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> partie;
+    ArrayList<Joueur> partieList = new ArrayList<>();
     ArrayAdapter<String> arrayAdapter;
     ListView listView;
     EditText editText;
     Button buttonAdd;
+    Button buttonJouer;
     int i = 0;
 
     @Override
@@ -31,16 +33,30 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.teamView);
         editText = findViewById(R.id.edit);
         buttonAdd = findViewById(R.id.ajouter);
+        buttonJouer = findViewById(R.id.jouer);
 
         partie = new ArrayList<>();
         arrayAdapter = new ArrayAdapter<>(this, R.layout.player_view_layout,partie);
         listView.setAdapter(arrayAdapter);
-
+        buttonJouer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createTeam();
+            }
+        });
     }
 
     public void addPlayerToTeam(View v){
         partie.add(editText.getText().toString());
         arrayAdapter.notifyDataSetChanged();
         editText.setText("");
+    }
+
+    public void createTeam(){
+        for(int i=0;i<partie.size();i++){
+            Joueur jtemp = new Joueur(i, partie.get(i));
+            partieList.add(jtemp);
+            Log.d("Main",""+partieList.get(i).toString());
+        }
     }
 }
