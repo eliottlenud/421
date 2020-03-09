96a5f40cb32f33500e140a0b9b8b13f7233f7786
Virgile ALLARD <virgile.allard@hotmail.fr>
2020-03-09 12:12:38 +0100

    shake it just 3 times

--- app/src/main/java/io/hei/a421/partieActivity.java
+++ app/src/main/java/io/hei/a421/partieActivity.java
@@ -2,6 +2,7 @@ package io.hei.a421;
 
 import android.app.AppComponentFactory;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -30,7 +31,11 @@ public class partieActivity extends AppCompatActivity {
     public int rangJoueur; //Pour parcourir la liste des joueurs
     private SensorManager mSensorManager = null;
     private Sensor mAccelerometer = null;
-    private static int SHAKE_THRESHOLD = 3;
+    private static int SHAKE_THRESHOLD = 10;
+    public boolean moveit = true;
+    public int nbclick = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,7 @@ public class partieActivity extends AppCompatActivity {
         setContentView(R.layout.activity_party);
         LinearLayout rangee = (LinearLayout) findViewById(R.id.rangee);
         Button finDuTour = (Button) findViewById((R.id.finDuTour));
+        final Button relancer = (Button) findViewById((R.id.relancer));
         imageView1 = (ImageView) findViewById(R.id.imageView1);
         imageView2 = (ImageView) findViewById(R.id.imageView2);
         imageView3 = (ImageView) findViewById(R.id.imageView3);
@@ -99,6 +105,10 @@ public class partieActivity extends AppCompatActivity {
         finDuTour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                nbclick= 0;
+                relancer.setVisibility(View.VISIBLE);
+                moveit = true;
+                onResume();
                 rangJoueur ++;
                 if (rangJoueur == listeJoueurs.size()){
                     rangJoueur=0;
@@ -106,6 +116,19 @@ public class partieActivity extends AppCompatActivity {
                 nomJoueurActuel.setText(listeJoueurs.get(rangJoueur));
             }
 
+        });
+
+        relancer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveit = true;
+
+                onResume();
+                nbclick= nbclick +1;
+                if (nbclick == 2) {
+                    relancer.setVisibility(View.INVISIBLE);
+                }
+            }
         });
 
 
@@ -144,7 +167,13 @@ public class partieActivity extends AppCompatActivity {
             System.out.println("z=" + z);
             float acceleration = (float) Math.sqrt(x*x + y*y + z*z) - SensorManager.GRAVITY_EARTH;
 
+            shakeoupasshake();
+
             if (acceleration > SHAKE_THRESHOLD) {
+                moveit = false;
+                int value1 = randomDiceValue();
+                int value2 = randomDiceValue();
+                int value3 = randomDiceValue();
                 final Animation.AnimationListener animationListener = new Animation.AnimationListener() {
                     @Override
                     public void onAnimationStart(Animation animation) {
@@ -185,6 +214,12 @@ public class partieActivity extends AppCompatActivity {
 
     public static int randomDiceValue() {
         return RANDOM.nextInt(6) + 1;
+    }
+
+    public void shakeoupasshake() {
+        if (moveit == false) {
+            onPause();
+        }
     }
 
 }
