9617c76c5d991edec2fb5062f788c44faf7e8a25
Juliette Favoulet <juliette.favoulet@yahoo.fr>
2020-03-09 11:04:59 +0100

    Activity menuRegles

---
+++ app/src/main/java/io/hei/a421/menuRegles.java
@@ -0,0 +1,13 @@
+package io.hei.a421;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class menuRegles extends AppCompatActivity {
+
+    protected void OnCreate(Bundle savedInstantiateState){
+        super.onCreate(savedInstantiateState);
+        setContentView(R.layout.reglesjeu_layout);
+    }
+}
