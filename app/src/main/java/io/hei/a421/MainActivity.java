e9d3cdb604b2358494b93eafc69c1a06c2c75fa4
PoloScolo <56428626+PoloScolo@users.noreply.github.com>
2020-03-10 11:39:03 +0100

    Merge branch 'master' of C:\Users\Paul Scoliege\Documents\HEI4\ProgMobile\421\421 with conflicts.

--- app/src/main/java/io/hei/a421/MainActivity.java
+++ app/src/main/java/io/hei/a421/MainActivity.java
@@ -1,12 +1,8 @@
 package io.hei.a421;
 
-import androidx.annotation.ColorInt;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,8 +10,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -24,75 +18,40 @@ import io.hei.a421.Models.Joueur;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> partie;
-    public ArrayList<Joueur> partieList = new ArrayList<>();
+    ArrayList<Joueur> partieList = new ArrayList<>();
     ArrayAdapter<String> arrayAdapter;
     ListView listView;
     EditText editText;
-    Button buttonAdd, buttonJouer, buttonRegles, buttonReset;
+    Button buttonAdd, buttonJouer, buttonRegles;
     int i = 0;
-    String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.teamView);
 
+        listView = findViewById(R.id.teamView);
         editText = findViewById(R.id.edit);
         buttonAdd = findViewById(R.id.ajouter);
         buttonJouer = findViewById(R.id.jouer);
         buttonRegles = findViewById(R.id.regles);
-        buttonReset = findViewById(R.id.reset);
 
         partie = new ArrayList<>();
-        arrayAdapter = new ArrayAdapter<>(this, R.layout.player_box, R.id.pseudo, partie);
-        TextView pseudo = findViewById(R.id.pseudo);
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.player_view_layout,partie);
         listView.setAdapter(arrayAdapter);
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(editText.getText().length()!=0){
-                    addPlayerToTeam(v);
-                }
-                else {
-                    Log.d("coucou2","ca rentre la2");
-                    final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-                    alertDialogBuilder.setMessage("Vous devez rentrer un pseudo !");
-                    alertDialogBuilder.setPositiveButton("Compris !", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Toast.makeText(getApplicationContext(),"mkay bro",
-                                    Toast.LENGTH_SHORT).show();
-                            dialog.cancel();
-                        }
-                    });
-                    AlertDialog alertDialog = alertDialogBuilder.create();
-                    alertDialog.show();
-                }
-            }
-        });
         buttonJouer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 createTeam();
-                Intent intent = new Intent(MainActivity.this, partieActivity.class);
-                intent.putParcelableArrayListExtra("partieList", partieList);
+                Intent intent=new Intent(MainActivity.this, partieActivity.class);
                 startActivity(intent);
             }
         });
         buttonRegles.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Test","MenuRegles - intent");
-                Intent intent = new Intent(MainActivity.this, menuRegles.class);
-                startActivity(intent);
-            }
-        });
-        buttonReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetTeam();
+                setContentView(R.layout.reglesjeu_layout);
             }
         });
     }
@@ -107,14 +66,8 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0;i<partie.size();i++){
             Joueur jtemp = new Joueur(i, partie.get(i));
             partieList.add(jtemp);
-            Log.d(TAG,""+partieList.get(i).toString());
+            Log.d("Main",""+partieList.get(i).toString());
         }
-    }
-
-    public void resetTeam(){
-        int i=0;
-        partie.clear();
-        arrayAdapter.notifyDataSetChanged();
     }
 
 }
