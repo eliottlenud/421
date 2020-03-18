af9706d5107a7f5e7632658c58392cae592357ee
Hugues Barry <hugues.barryf@hei.yncrea.fr>
2020-03-18 12:25:25 +0100

    Lien Menu / jeu

--- app/src/main/java/io/hei/a421/partieActivity.java
+++ app/src/main/java/io/hei/a421/partieActivity.java
@@ -2,6 +2,8 @@ package io.hei.a421;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -20,47 +22,57 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 import io.hei.a421.Models.Joueur;
 
 public class partieActivity extends AppCompatActivity {
 
+/*--------------------------------------------DECLARATION DES VARIABLES------------------------------------------------*/
     public static final Random RANDOM = new Random();
-    public ImageView imageView1, imageView2, imageView3; //Les 3 images des dès
-    public TextView nomJoueurActuel, numberofjetons, valeur1,valeur2,valeur3; //Nom du joueur qui joue
-    public int rangJoueur; //Pour parcourir la liste des joueurs
-    private SensorManager mSensorManager = null;
+    public ImageView imageView1, imageView2, imageView3;        //Les 3 images des dés
+    public TextView nomJoueurActuel, numberofjetons, topScore, minScore;  //Nom du joueur qui joue
+    public int rangJoueur;                                      //Pour parcourir la liste des joueurs
+    private SensorManager mSensorManager = null;                //Pour lancer les dés en secouant l'appareil
     private Sensor mAccelerometer = null;
-    public ArrayList<Joueur> partieList;
     private static int SHAKE_THRESHOLD = 10;
-    public boolean moveit = true;
-    public int nbclick = 0;
-    int a=0,b=0,c=0;
+    public ArrayList<Joueur> partieList;                        //Liste des joueurs de la partie
+    public boolean moveit = true;                               //Pour mettre en pause entre 2 lancers
+    public int nbclick = 0;                                     //Gère le nombre de lancers par personne
+    int a=0,b=0,c=0;                                            //Gère le verrouillage des dés (à ne pas relancer)
     boolean verouillage_1,verouillage_2,verouillage_3;
+    int value, value2, value3, score;                                  //Les chiffres composant le score
     MediaPlayer sonDe;
     String TAG = "partieActivity";
+    List<Integer> listeScore = Tableau.listeScores;             //Liste et tableau des scores possibles avec nb de jetons correspondant
+    int[][] tableau = Tableau.tableau;
+    ArrayList <Integer> listeIndex = new ArrayList<>();         //Liste qui va servir à regarder meilleur/pire score
 
 
-
+/*-------------------------------------------------A L'OUVERTURE DU JEU-------------------------------------------------*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_party);
         LinearLayout rangee =  findViewById(R.id.rangee);
         Button finDuTour =  findViewById((R.id.finDuTour));
+        Button lancer =  findViewById((R.id.lancer));
         final Button relancer =  findViewById((R.id.relancer));
 
-        //Définition des 3 dés + Nom joueur
+        //Définition des 3 dés + Nom joueur + Nb de jetons du joueur
         imageView1 =  findViewById(R.id.imageView1);
         imageView2 =  findViewById(R.id.imageView2);
         imageView3 =  findViewById(R.id.imageView3);
-        valeur1 = findViewById(R.id.valeur1);
-        valeur2 = findViewById(R.id.valeur2);
-        valeur3 = findViewById(R.id.valeur3);
         nomJoueurActuel =  findViewById(R.id.nomJoueurActuel);
         numberofjetons = findViewById(R.id.numberofjetons);
 
-        //sonDe = MediaPlayer.create(this, R.raw.son);
+        //Définition des scores
+        topScore = findViewById(R.id.topScore);
+        minScore = findViewById(R.id.toBeatScore);
+
+        sonDe = MediaPlayer.create(this, R.raw.son);
 
         //Sensor
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -88,9 +100,9 @@ public class partieActivity extends AppCompatActivity {
 
 
 
-/*---------TEST EN CLIQUANT SUR LES DES----------*/
-//Lancer les dés
-        /*rangee.setOnClickListener(new View.OnClickListener() {
+        /*------------------------------TEST EN CLIQUANT SUR LES DES--------------------*/
+        //Lancer les dés
+        lancer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //sonDe.start();
@@ -104,9 +116,10 @@ public class partieActivity extends AppCompatActivity {
 
                     @Override
                     public void onAnimationEnd(Animation animation) {
-                        int value = randomDiceValue();
-                        int value2 = randomDiceValue();
-                        int value3 = randomDiceValue();
+
+                        if (verouillage_1==false){ value = randomDiceValue();}
+                        if (verouillage_2==false){ value2 = randomDiceValue();}
+                        if (verouillage_3==false){ value3 = randomDiceValue();}
                         int temp;
 
                         //Trie les dés dans l'ordre décroissant
@@ -123,6 +136,12 @@ public class partieActivity extends AppCompatActivity {
                             }
                         }
 
+                        System.out.println("Value 1 = "+value);
+                        System.out.println("Value 2 = "+value2);
+                        System.out.println("Value 3 = "+value3);
+                        score = value*100 + value2 *10 + value3;
+                        System.out.println("Nombre de jetons = "+tableau[listeScore.indexOf(score)][1]);
+
                         int res = getResources().getIdentifier("dice_" + value, "drawable", "io.hei.a421");
                         int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "io.hei.a421");
                         int res3 = getResources().getIdentifier("dice_" + value3, "drawable", "io.hei.a421");
@@ -130,6 +149,16 @@ public class partieActivity extends AppCompatActivity {
                         imageView1.setImageResource(res);
                         imageView2.setImageResource(res2);
                         imageView3.setImageResource(res3);
+
+                        a = 0;
+                        b = 0;
+                        c = 0;
+                        verouillage_1 = false;
+                        verouillage_2 = false;
+                        verouillage_3 = false;
+                        imageView1.setBackgroundColor(Color.TRANSPARENT);
+                        imageView2.setBackgroundColor(Color.TRANSPARENT);
+                        imageView3.setBackgroundColor(Color.TRANSPARENT);
                     }
 
                     @Override
@@ -143,60 +172,58 @@ public class partieActivity extends AppCompatActivity {
                 imageView2.startAnimation(anim1);
                 imageView3.startAnimation(anim1);
             }
-
-
-
         }
-        );*/
-/*-------------------------VERROUILLAGE DES DES--------------------------*/
-        //verouiller le dé 1
+        );
+/*------------------------------------------------------VERROUILLAGE DES DES--------------------------------------------------*/
+        //verrouiller le dé 1
         imageView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (a==0) {
-                    imageView1.setBackgroundResource(R.drawable.fond_vert_de);
+                    imageView1.setBackgroundColor(Color.RED);
                     verouillage_1 =true;
                     a=1;
                 }else {
-                    imageView1.setBackgroundResource(R.drawable.tapis);
+                    imageView1.setBackgroundColor(Color.TRANSPARENT);
                     verouillage_1 =false;
                     a=0;
                 }
             }
         });
 
-        //verouiller le dé 2
+        //verrouiller le dé 2
         imageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (b==0) {
-                    imageView2.setBackgroundResource(R.drawable.fond_vert_de);
+                    imageView2.setBackgroundColor(Color.RED);
                     verouillage_2 =true;
                     b=1;
                 }else {
-                    imageView2.setBackgroundResource(R.drawable.tapis);
+                    imageView2.setBackgroundColor(Color.TRANSPARENT);
                     verouillage_2 =false;
                     b=0;
                 }
             }
         });
 
-        //verouiller le dé 3
+        //verrouiller le dé 3
         imageView3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (c==0) {
-                    imageView3.setBackgroundResource(R.drawable.fond_vert_de);
+                    imageView3.setBackgroundColor(Color.RED);
                     verouillage_3 =true;
                     c=1;
                 }else {
-                    imageView3.setBackgroundResource(R.drawable.tapis);
+                    imageView3.setBackgroundColor(Color.TRANSPARENT);
                     verouillage_3 =false;
                     c=0;
                 }
             }
         });
 
+/*-----------------------------------------BOUTON FIN DU TOUR----------------------------------------------------*/
         //Bouton qui passe au joueur suivant
         finDuTour.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -211,20 +238,27 @@ public class partieActivity extends AppCompatActivity {
                 }
                 nomJoueurActuel.setText(partieList.get(rangJoueur).getPseudo());
                 numberofjetons.setText(""+partieList.get(rangJoueur).getNbJetons());
+                listeIndex.add(Tableau.listeScores.indexOf(score));
+                topScore.setText(""+Tableau.listeScores.get(Collections.min(listeIndex)));
+                minScore.setText(""+Tableau.listeScores.get(Collections.max(listeIndex)));
+                System.out.println("La liste des scores est "+listeIndex);
             }
 
         });
 
+/*-----------------------------------------BOUTON RELANCER----------------------------------------------------*/
         //Bouton qui permet au joueur actuel de relancer (max 3 lancers)
         relancer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                moveit = true;
+                if (moveit == false) {
+                    moveit = true;
 
-                onResume();
-                nbclick= nbclick +1;
-                if (nbclick == 2) {
-                    relancer.setVisibility(View.INVISIBLE);
+                    onResume();
+                    nbclick = nbclick + 1;
+                    if (nbclick == 2) {
+                        relancer.setVisibility(View.INVISIBLE);
+                    }
                 }
             }
         });
@@ -259,9 +293,6 @@ public class partieActivity extends AppCompatActivity {
             float y = sensorEvent.values[1];
             float z = sensorEvent.values[2];
 
-            System.out.println("x=" + x);
-            System.out.println("y=" + y);
-            System.out.println("z=" + z);
             float acceleration = (float) Math.sqrt(x*x + y*y + z*z) - SensorManager.GRAVITY_EARTH;
             shakeoupasshake();
 
@@ -274,9 +305,13 @@ public class partieActivity extends AppCompatActivity {
 
                     @Override
                     public void onAnimationEnd(Animation animation) {
-                        int value = randomDiceValue();
-                        int value2 = randomDiceValue();
-                        int value3 = randomDiceValue();
+                        System.out.println("Quand je secoue");
+                        System.out.println("verrouillage 1 = "+verouillage_1);
+                        System.out.println("verrouillage 2 = "+verouillage_2);
+                        System.out.println("verrouillage 3 = "+verouillage_3);
+                        if (verouillage_1==false){ value = randomDiceValue();}
+                        if (verouillage_2==false){ value2 = randomDiceValue();}
+                        if (verouillage_3==false){ value3 = randomDiceValue();}
                         int temp;
 
                         //Trie les dés dans l'ordre décroissant
@@ -293,13 +328,29 @@ public class partieActivity extends AppCompatActivity {
                             }
                         }
 
+                        System.out.println("Value 1 = "+value);
+                        System.out.println("Value 2 = "+value2);
+                        System.out.println("Value 3 = "+value3);
+                        score = value*100 + value2 *10 + value3;
+
                         int res = getResources().getIdentifier("dice_" + value, "drawable", "io.hei.a421");
                         int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "io.hei.a421");
                         int res3 = getResources().getIdentifier("dice_" + value3, "drawable", "io.hei.a421");
 
-                        if (verouillage_1==false){ imageView1.setImageResource(res);}
-                        if (verouillage_2==false){ imageView2.setImageResource(res2);}
-                        if (verouillage_3==false){ imageView3.setImageResource(res3);}
+                        imageView1.setImageResource(res);
+                        imageView2.setImageResource(res2);
+                        imageView3.setImageResource(res3);
+
+                        a = 0;
+                        b = 0;
+                        c = 0;
+                        verouillage_1 = false;
+                        verouillage_2 = false;
+                        verouillage_3 = false;
+                        imageView1.setBackgroundColor(Color.TRANSPARENT);
+                        imageView2.setBackgroundColor(Color.TRANSPARENT);
+                        imageView3.setBackgroundColor(Color.TRANSPARENT);
+
                     }
 
                     @Override
@@ -312,6 +363,8 @@ public class partieActivity extends AppCompatActivity {
                 imageView1.startAnimation(anim1);
                 imageView2.startAnimation(anim1);
                 imageView3.startAnimation(anim1);
+
+
 
             }
 
